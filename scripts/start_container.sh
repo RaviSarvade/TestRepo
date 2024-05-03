0aed3f812b01f42a540414ae73ac791696f84837
RaviSarvade <83575386+RaviSarvade@users.noreply.github.com>
2024-05-03 18:54:02 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,5 +5,5 @@ set -e
 echo
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 --name simple_python 580194822445.dkr.ecr.us-east-1.amazonaws.com/simple-python-application:latest
+docker run -d -p 5000:5000 --name simple_python raqvisarvade/simple-python-application:latest
 
