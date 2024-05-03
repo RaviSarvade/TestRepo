2e08f6e4eef12251ba52a7f5e25050838e43fbb2
RaviSarvade <83575386+RaviSarvade@users.noreply.github.com>
2024-05-03 19:18:54 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,7 +3,6 @@ set -e
 
 # Pull the Docker image from Docker Hub
 echo
-
 # Run the Docker image as a container
 docker run -d -p 5000:5000 --name simple_python raqvisarvade/simple-python-application:latest
 
